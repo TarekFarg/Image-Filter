20d938224148de02c70cf552b3887b0d26001539
3laa hassan <3laahassan106@gmail.com>
2023-10-18 18:13:16 +0300

    Filters: inverted, Rotate Image, Enlarge Image, Shuffle Image and Skew Image done

--- Assignment_one_oop.cpp
+++ Assignment_one_oop.cpp
@@ -43,7 +43,7 @@ int main() {
     cout<<"Ahlan ya user ya habibi"<<endl;
     loadImage();
     int choice=1;
-    cout<<"1-Black & White Filter\n2-Invert Filter\n3-Merge Filter \n4-Flip Image\n5-Rotate Image \n6-Darken and Lighten Image\n7-Detect Image Edges \n8-Enlarge Image\n9-Shrink Image\n10-Mirror 1/2 Image\n11-Shuffle Image\n12-Blur Image\n13-Crop Image\n14-Skew Image Right  \n15-Skew Image Up  \n16-Save the image to a file\n";
+    cout<<"1-Black & White Filter\n2-Invert Filter\n3-Merge Filter \n4-Flip Image\n5-Rotate Image \n6-Darken and Lighten Image\n7-Detect Image Edges \n8-Enlarge Image\n9-Shrink Image\n10-Mirror 1/2 Image\n11-Shuffle Image\n12-Blur Image\n13-Crop Image\n14-Skew Image Horizontal  \n15-Skew Image Vertical \n16-Save the image to a file\n";
     while (choice!=0){
         cout<<"Please select a filter to apply or 16 to save or 0 to exit: \n";
         cin>>choice;
@@ -90,7 +90,9 @@ int main() {
                 Crop_Image() ;
                 break;
             case 14:
-                Skew_Image();
+                void Skew_Image_Horizontal();
+            case 15:
+                void Skew_Image_Vertical();
             case 16:
                 saveImage();
                 break;
@@ -163,7 +165,11 @@ void BlackandWhite()  {
 }
 
 void InvertImage(){
-    // code
+        for (int i = 0; i < SIZE ; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                image[i][j]=255-image[i][j];
+        }
+    }
 }
 
 void MergeImages(){
@@ -205,7 +211,45 @@ void  Flip_Image()
 }
 
 void RotateImage(){
-    // code
+    cout<<"Rotate (90), (180) or (270) degrees?\n";
+    int degree;cin>>degree;
+    char NewImage[SIZE][SIZE];
+    if(degree==90){
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                NewImage[i][j]=image[SIZE-1-j][i];
+            }
+        }
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                image[i][j]=NewImage[i][j];
+            }
+        }
+    }
+    else if(degree==180){
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                NewImage[i][j]=image[SIZE-1-j][i];
+            }
+        }
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                image[i][j]=NewImage[SIZE-1-j][i];
+            }
+        }
+    }
+    else{
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                NewImage[i][j]=image[j][SIZE-1-i];
+            }
+        }
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                image[i][j]=NewImage[i][j];
+            }
+        }
+    }
 }
 
 void Detect_Image_Edges()
@@ -227,7 +271,38 @@ void Detect_Image_Edges()
 }
 
 void EnlargeImage(){
-    // code
+ cout<<"Which quarter to enlarge 1, 2, 3 or 4?\n";
+    int x,a=0,b=128;
+    unsigned char quarter[SIZE/2][SIZE/2];
+    cin>>x;
+    if(x > 2){      //quarter 3 or 4
+        a=128;
+    }
+    if(x%2){        //quarter 1 or 3
+        b=0;
+    }
+    for (int i = 0; i < SIZE/2; ++i) {          //get quarter data
+        for (int j = 0; j < SIZE/2; ++j) {
+            quarter[i][j]=image[i+a][j+b];
+        }
+    }
+
+    for (int i = 0; i < SIZE/2 ; ++i) {          //enlarge quarter
+        for (int j = 0; j < SIZE/2 ; ++j) {
+            SecImage[2*i][2*j]=quarter[i][j];
+        }
+    }
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 1; j < SIZE; j+=2) {
+            SecImage[i][j]=(SecImage[i][j-1]+SecImage[i][j+1])/2;
+        }
+    }
+    for (int i = 1; i < SIZE; i+=2) {
+        for (int j = 0; j < SIZE; j++) {
+            SecImage[i][j]=(SecImage[i-1][j]+SecImage[i+1][j])/2;
+        }
+    }
+    copyImageToSec();
 }
 
 void ShrinkImage(){
@@ -314,7 +389,68 @@ void Mirror_half_Image()
 
 void Shuffle_Image()
 {
-    // code
+  cout << "New order of quarters ?\n" ;
+    int order ; // for new order
+    int x , y , tempy; // index of SecImage
+    for (int i = 0; i < 4; ++i)
+    {
+        cin >> order ;
+        if(i==0)
+            x = 0 , tempy = 0 ; // quarter 1
+        else if(i==1)
+            x = 0 , tempy = SIZE/2 ; // quarter 2
+        else if(i==2)
+            x = SIZE/2 , tempy = 0; // quarter 3
+        else if(i == 3)
+            x = SIZE/2 , tempy = SIZE/2 ; // quarter 4
+
+        if(order == 1)
+        {
+            for (int j = 0; j < SIZE / 2; ++j) {
+                y =tempy ;
+                for (int k = 0; k < SIZE / 2; ++k) {
+                    SecImage[x][y] = image[j][k] ;
+                    y++ ;
+                }
+                x++ ;
+            }
+        }
+        else if(order == 2)
+        {
+            for (int j = 0; j < SIZE / 2; ++j) {
+                y =tempy ;
+                for (int k = SIZE / 2; k < SIZE ; ++k) {
+                    SecImage[x][y] = image[j][k] ;
+                    y++ ;
+                }
+                x++ ;
+            }
+        }
+        else if(order == 3)
+        {
+            for (int j = SIZE / 2; j < SIZE ; ++j) {
+                y =tempy ;
+                for (int k = 0 ; k < SIZE / 2 ; ++k) {
+                    SecImage[x][y] = image[j][k] ;
+                    y++ ;
+                }
+                x++ ;
+            }
+        }
+        else if(order == 4)
+        {
+            for (int j = SIZE / 2; j < SIZE ; ++j) {
+                y =tempy ;
+                for (int k = SIZE / 2 ; k < SIZE  ; ++k) {
+                    SecImage[x][y] = image[j][k] ;
+                    y++ ;
+                }
+                x++ ;
+            }
+        }
+    }
+    copyImageToSec();//make image = SecImage
+
 }
 
 void BlurImage(){
@@ -361,8 +497,52 @@ void  Crop_Image()
         }
     }
 }
-void Skew_Image(){
-    // code
+void Skew_Image_Horizontal(){
+cout << "please enter the angel \n" ;
+    double ang ; cin >> ang ;
+    ang = tan(ang*3.14159/180) ;
+    int p =  round(1/ang) ;
+
+    int move = 256 * ang , cur ;
+    for (int i = 0; i < SIZE ; ++i) {          //Shrink
+        for (int j = 0; j < SIZE ; ++j) {
+            SecImage[i*(256-move)/256][j]=image[i][j];
+        }
+    }
+    for (int i = 0; i < SIZE; ++i) {            // to  skew the image
+
+        for (int j = 0; j < SIZE ; ++j) {
+            cur = (256-j)*ang ;
+            if(i>=cur && i < SIZE-(move-cur))
+                image[i][j] = SecImage[i-cur][j] ;
+            else
+                image[i][j] = 255 ;
+        }
+    }
+
+}
+void Skew_Image_Vertical(){
+      cout << "please enter the angel \n" ;
+    double ang ; cin >> ang ;
+    ang = tan(ang*3.14159/180) ;
+    int p =  round(1/ang) ;
+
+    int move = 256 * ang , cur ;
+    for (int i = 0; i < SIZE ; ++i) {          //Shrink
+        for (int j = 0; j < SIZE ; ++j) {
+            SecImage[i][j*(256-move)/256]=image[i][j];
+        }
+    }
+    for (int i = 0; i < SIZE; ++i) {            // to  skew the image
+        cur = (256-i)*ang ;
+        for (int j = 0; j < SIZE ; ++j) {
+            if(j>=cur && j < SIZE-(move-cur))
+                image[i][j] = SecImage[i][j-cur] ;
+            else
+                image[i][j] = 255 ;
+        }
+    }
+
 }
 
 
